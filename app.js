bd9bb328de553ff92efc8efea6413afd4ac8015d
ndraa124 <indradavidpesik@gmail.com>
2021-01-09 15:33:13 +0800

    update app

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(bodyParser.urlencoded({
 
 // routes images
 app.use('/images', express.static('./uploads/'))
+app.use('/image', express.static('./public/images'))
 
 // routes access
 app.use('/', indexRouter)
